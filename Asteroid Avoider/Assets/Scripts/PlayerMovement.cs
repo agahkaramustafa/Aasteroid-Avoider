446b0fe9857ce94e378df4e277b6b5322d6a5331
Agah Karamustafa <96539896+agahkaramustafa@users.noreply.github.com>
2023-04-04 16:41:42 +0300

    Update PlayerMovement.cs
    -added screen wraparound

--- Asteroid Avoider/Assets/Scripts/PlayerMovement.cs
+++ Asteroid Avoider/Assets/Scripts/PlayerMovement.cs
@@ -21,7 +21,13 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        if(!Touchscreen.current.press.isPressed) {return;}
+        ProcessInput();
+        KeepPlayerOnScreen();
+    }
+
+    void ProcessInput()
+    {
+        if (!Touchscreen.current.press.isPressed) { return; }
 
         Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
 
@@ -30,6 +36,32 @@ public class PlayerMovement : MonoBehaviour
         movementDirection = transform.position - worldPosition;
         movementDirection.z = 0f;
         movementDirection.Normalize();
+    }
+
+    void KeepPlayerOnScreen()
+    {
+        Vector3 newPosition = transform.position;
+        Vector3 viewPortPosition = mainCamera.WorldToViewportPoint(transform.position);
+
+        if (viewPortPosition.x > 1)
+        {
+            newPosition.x = -newPosition.x + 0.1f;
+        }
+        else if (viewPortPosition.x < 0)
+        {
+            newPosition.x = -newPosition.x - 0.1f;
+        }
+
+        if (viewPortPosition.y > 1)
+        {
+            newPosition.y = -newPosition.y + 0.1f;
+        }
+        else if (viewPortPosition.y < 0)
+        {
+            newPosition.y = -newPosition.y - 0.1f;
+        }
+
+        transform.position = newPosition;
     }
 
     void FixedUpdate() 
